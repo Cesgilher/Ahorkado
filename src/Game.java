8307b3dcc023a93646cb96e75b31fb57e52b783e
Jagoba <jsantistebanabe@gmail.com>
2023-11-08 13:43:54 +0100

    menu juego

--- src/Game.java
+++ src/Game.java
@@ -3,50 +3,35 @@ import java.util.Scanner;
 
 public class Game {
 
-    ArrayList<String> gameWord = new ArrayList<String>();
+    ArrayList<String> gameWord = new ArrayList<>();
     Scanner sc = new Scanner(System.in);
-
-    public void menu(){
-
-        int option;
-
-
-            do {
-                System.out.println("1- Jugar\n" +
-                                    "2- Salir");
-                try{
-
-
-                    option = sc.nextInt();
-                    sc.nextLine();
-
-                    if (option == 1){
-
-                        gameMenu();
-
-                    }else{
-                        System.out.println("Adios");
-                    }
-                }catch(java.util.InputMismatchException e) {
-                    System.out.println("Error: Ingresa un número válido.");
-                    sc.nextLine();
-                    option = -1;
-                }
-
-                }while (option != 0);
-
-    }
+    int level;
 
     public void gameMenu(){
         int gameOption;
 
         do {
             System.out.println("1- Elegir Nivel\n" +
-                    "2- Salir");
+                                "2- Jugar\n" +
+                                "3- Salir");
 
             try{
                 gameOption = sc.nextInt();
                 sc.nextInt();
+
+                switch (gameOption){
+
+                    case 1:
+                        selectLevel();
+                        break;
+                    case 2:
+                        play();
+                        break;
+                    case 3:
+
+                        break;
+
+                }
 
 
             }catch(java.util.InputMismatchException e){
@@ -59,5 +44,12 @@ public class Game {
         }while(gameOption != 0);
 
     }
+
+    private void play() {
+    }
+
+    private void selectLevel() {
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         Game game = new Game();
+        Scanner sc = new Scanner(System.in);
+        int option;
 
-        game.menu();
+
+        do {
+            System.out.println("1- Jugar\n" +
+                    "2- Salir");
+            try{
+
+
+                option = sc.nextInt();
+                sc.nextLine();
+
+                if (option == 1){
+
+                    game.gameMenu();
+
+                }else{
+                    System.out.println("Adios");
+                }
+            }catch(java.util.InputMismatchException e) {
+                System.out.println("Error: Ingresa un número válido.");
+                sc.nextLine();
+                option = -1;
+            }
+
+        }while (option != 0);
 
     }
 }
