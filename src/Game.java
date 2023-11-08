6d75152604ce0bce6fca96073b28adc79ba06a10
César Hernando Hernando <cehernando@es.gestamp.com>
2023-11-08 14:16:17 +0100

    arereglo main

--- src/Game.java
+++ src/Game.java
@@ -19,8 +19,6 @@ public class Game {
     }
 
 
-    public void selectLevel() {
-    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,52 +2,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Game game;
+        Game game = new Game();
 
         Scanner sc = new Scanner(System.in);
         int level;
 
         int gameOption;
 
-        do {
-            System.out.println("1- Elegir Nivel\n" +
-                    "2- Jugar\n" +
-                    "3- Salir");
 
-            try{
-                gameOption = sc.nextInt();
+        System.out.println("1- Elegir Nivel\n" +
+                "2- Jugar\n");
 
-
-                switch (gameOption){
-
-                    case 1:
-                        System.out.println("introducir nivel\n" +
-                                "1- facil\n" +
-                                "2- normal\n" +
-                                "3- dificil");
-                        level = sc.nextInt();
+        try{
+            gameOption = sc.nextInt();
 
 
-                        game = new Game(level);
-                        break;
-                    case 2:
-                        game = new Game();
-                        break;
-                    case 3:
+            switch (gameOption){
 
-                        break;
+                case 1:
+                    System.out.println("introducir nivel\n" +
+                            "1- facil\n" +
+                            "2- normal\n" +
+                            "3- dificil");
+                    level = sc.nextInt();
+                    game = new Game(level);
+                    break;
+                case 2:
 
-                }
+                    break;
 
-
-            }catch(java.util.InputMismatchException e){
-                System.out.println("Error: Ingresa un número válido.");
-                sc.nextLine();
-                gameOption = -1;
             }
 
 
-        }while(gameOption != 0);
+        }catch(java.util.InputMismatchException e){
+            System.out.println("Error: Ingresa un número válido.");
+            sc.nextLine();
+            gameOption = -1;
+        }
+        game.play();
+
+
+
 
     }
 }
