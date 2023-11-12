8553c30b868024f7c5e017bbb5814e267576bd9d
Your Name <you@example.com>
2023-11-12 21:55:14 +0100

    asi se queda

--- src/Game.java
+++ src/Game.java
@@ -21,40 +21,45 @@ public class Game extends JFrame{
 
     public Game(){
         this.level = 0;
-    }
-    public Game(int level ){
-        this.level = level;
+        Init();
+
     }
 
 
-    public void ShowWord(){
-        System.out.print("La palabra es: " + gameWord);
-    }
+
 
     public void CheckChar() {
+        String guess = inputField.getText().toUpperCase(); // Convert the input to uppercase
 
-        String guess = inputField.getText().toUpperCase();
         if (guess.length() != 1 || !Character.isLetter(guess.charAt(0))) {
             resultLabel.setText("Ingresa una letra válida.");
             CheckChar();
         } else {
             char letter = guess.charAt(0);
-            if (guessedChars.contains(letter) | failedChars.contains(letter)){
+            letter = Character.toUpperCase(letter); // Ensure the comparison is case-insensitive
+
+            if (guessedChars.contains(letter) || failedChars.contains(letter)) {
                 resultLabel.setText("Ya has usado esa letra, ingresa otra.");
                 CheckChar();
-            }
-            else if (gameWord.contains(String.valueOf(letter))) {
+            } else if (gameWord.toUpperCase().contains(String.valueOf(letter))) {
                 guessedChars.add(letter);
                 resultLabel.setText("¡Letra correcta!");
-
             } else {
                 failedChars.add(letter);
                 attempts++;
                 resultLabel.setText("¡Letra incorrecta! Intentos restantes: " + (4 - attempts));
             }
         }
+
+        UpdateWordLabel();
         inputField.setText("");
 
+    }
+
+    private void UpdateWordLabel() {
+        wordLabel.setText(ShowGuessedChars());
+        revalidate();
+        repaint();
     }
     public String ShowGuessedChars(){
         String word = "";
@@ -70,61 +75,94 @@ public class Game extends JFrame{
         return word;
 
     }
-    public void gameStatus(int attempts){
-
-        if (attempts==4){
-            resultLabel.setText("Has perdido, la palabra era: " + gameWord);
-
-        } else{
-            resultLabel.setText("¡Felicidades! Has ganado.");
+    public boolean RoundIsEnded() {
+        if (attempts == 4) {
+            return true;
+        } else {
+            // Check if all letters of the word are guessed
+            boolean allLettersGuessed = true;
+            for (int i = 0; i < gameWord.length(); i++) {
+                char c = gameWord.charAt(i);
+                if (!guessedChars.contains(c)) {
+                    allLettersGuessed = false;
+                    break; // Break out of the loop as soon as a non-guessed letter is found
+                }
+            }
+            return allLettersGuessed;
         }
     }
-    public void mostrarJuego() {
+
+    public void EndRound(Boolean failedRound){
+
+        if (failedRound){
+
+        }
+
+    }
+    public void Init() {
         setTitle("Juego del Ahorcado");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 200);
         setLayout(new GridLayout(4, 1));
+        InitRound();
+        setVisible(true);
 
-        initRound();
     }
-    private void initRound() { //Hay que añadir un menu con las diferentes opciones del juego( adivinar palabra y rendirse)
+    private void InitRound() { //Hay que añadir un menu con las diferentes opciones del juego( adivinar palabra y rendirse)
+        level++;
         gameWord = wm.GetRandomWord(level);
         attempts = 0;
         guessedChars.clear();
         failedChars.clear();
 
         wordLabel = new JLabel(ShowGuessedChars());
+        JButton guessButton = new JButton("Adivinar");
+        JButton surrenderButton = new JButton("Rendirte");
         add(wordLabel);
 
         inputField = new JTextField();
         add(inputField);
 
-        JButton guessButton = new JButton("Adivinar");
-        JButton surrenderButton = new JButton("Rendirte");
+
 
         //los guessButtons hacen la funcion del menu de opciones
         guessButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 CheckChar();
+                UpdateWordLabel();
+                if(RoundIsEnded()){
+                    DisplayRoundResult();
+                    wm.DeleteWord(gameWord);
+                    InitRound();
+                }
             }
         });
         surrenderButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 attempts = 4;
-                gameStatus(attempts);
+                DisplayRoundResult();
+                wm.DeleteWord(gameWord);
+                InitRound();
+
             }
         });
         add(guessButton);
+        add(surrenderButton);
 
         resultLabel = new JLabel("");
         add(resultLabel);
 
-        gameStatus(attempts);
 
-        //cuando se acabe la ronda ( wm.DeleteWord(gameWord))
-        wm.DeleteWord(gameWord);
+    }
+
+    private void DisplayRoundResult() {
+        if (attempts == 4) {
+            resultLabel.setText("Has perdido esta ronda. La palabra era: " + gameWord);
+        } else {
+            resultLabel.setText("¡Felicidades! Has adivinado la palabra: " + gameWord);
+        }
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,12 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Game game = new Game();
-        Scanner sc = new Scanner(System.in);
-        int level;
-
-        int gameOption;
-
-
-        System.out.println("1- Elegir Nivel\n" +
-                "2- Jugar\n");
-
-        try{
-            gameOption = sc.nextInt();
-
-
-            switch (gameOption) {
-
-                case 1:
-                    System.out.println("introducir nivel\n" +
-                            "1- facil\n" +
-                            "2- normal\n" +
-                            "3- dificil");
-                    level = sc.nextInt();
-                    //game.level = level;
-
-                    break;
-                case 2:
-                    game.mostrarJuego();
-                    break;
-
-            }
-        }catch(java.util.InputMismatchException e){
-            System.out.println("Error: Ingresa un número válido.");
-
-
-        }
-
-        //game.Play();
-
-
-
-
-
-
+        SwingUtilities.invokeLater(() -> {
+            new Game();
+        });
     }
+
 }
